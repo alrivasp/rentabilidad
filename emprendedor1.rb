b2e813ae31f22643dac3041661929a7dc122902d
alrivasp <alvaro.rivas.p@gmail.com>
2020-05-06 17:37:35 -0400

    UP Desafio 2 completo

---
+++ emprendedor1.rb
@@ -0,0 +1,27 @@
+puts "*** Desafio Rentabilidad Emprendedor 1 ***"
+
+# Valor de precio producto
+pp = ARGV[0].to_i
+
+# Valor de numero de usuarios
+nu = ARGV[1].to_i
+
+# Valor gastos anuales
+ga = ARGV[2].to_i
+
+#Calculo de Utilidades
+utilidades = pp*nu-ga
+
+respuesta = nil
+
+#Calculo de Impuestos
+if utilidades > 0
+    #Respuesta si tiene Utilidades
+    utilidades = utilidades*0.35
+    respuesta = "Su negocio si es Rentable, este tendra utilidades por US$ #{utilidades.to_f} , segun valores ingresados "
+else
+    #Respuesta si no Tiene Utilidades
+    respuesta = "Sus Utilidades son Negativas por lo que no es Rentable US$ #{utilidades.to_f}"
+end
+
+puts respuesta
---
+++ emprendedor2.rb
@@ -0,0 +1,33 @@
+puts "*** Desafio Rentabilidad Emprendedor 2 ***"
+
+# Valor de precio producto
+pp = ARGV[0].to_i
+
+# Valor de numero de usuarios
+nu = ARGV[1].to_i
+
+# Valor de numero de usuarios Premiun
+nup = ARGV[2].to_i
+
+# Valor de numero de usuarios gratuitos
+nug = ARGV[3].to_i
+
+# Valor gastos anuales
+ga = ARGV[4].to_i
+
+#Calculo de Utilidades (Calcular cantidad de usuario)* precio venta - gastos
+utilidades = (nup*2+nu)*pp-ga
+
+respuesta = nil
+
+#Calculo de Impuestos
+if utilidades > 0
+    #Respuesta si tiene Utilidades
+    utilidades = utilidades*0.35
+    respuesta = "Su negocio si es Rentable, este tendra utilidades por US$ #{utilidades.to_f} , segun valores ingresados para #{nu + nup + nug} Usuarios "
+else
+    #Respuesta si no Tiene Utilidades
+    respuesta = "Sus Utilidades son Negativas por lo que no es Rentable US$ #{utilidades.to_f}"
+end
+
+puts respuesta
